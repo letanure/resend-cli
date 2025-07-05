dbbad7a7388262ca0f3fe0b5521915724c62b728
letanure <letanure@gmail.com>
2025-07-05 11:14:17 +0200

    feat: add Broadcasts Menu component

---
+++ src/broadcasts/BroadcastsMenu.tsx
@@ -0,0 +1,66 @@
+import { Menu, type MenuItem } from '../components/ui/Menu.js';
+
+export enum BroadcastsMenuState {
+	create = 'create',
+	retrieve = 'retrieve',
+	update = 'update',
+	send = 'send',
+	delete = 'delete',
+	list = 'list',
+}
+
+const BROADCASTS_MENU_ITEMS: Array<MenuItem<BroadcastsMenuState>> = [
+	{
+		id: BroadcastsMenuState.create,
+		label: 'Create',
+		description: 'Create a new broadcast to send to your audience.',
+	},
+	{
+		id: BroadcastsMenuState.retrieve,
+		label: 'Retrieve',
+		description: 'Retrieve a single broadcast..',
+	},
+	{
+		id: BroadcastsMenuState.update,
+		label: 'Update',
+		description: 'Update a broadcast to send to your audience.',
+	},
+	{
+		id: BroadcastsMenuState.send,
+		label: 'Send',
+		description: 'Start sending broadcasts to your audience through the Resend API.',
+	},
+	{
+		id: BroadcastsMenuState.delete,
+		label: 'Delete',
+		description: 'Remove an existing broadcast.',
+	},
+	{
+		id: BroadcastsMenuState.list,
+		label: 'List',
+		description: 'Retrieve a list of broadcast.',
+	},
+];
+
+interface BroadcastsMenuProps {
+	onExit: () => void;
+}
+
+export const BroadcastsMenu = ({ onExit }: BroadcastsMenuProps) => {
+	const handleMenuSelect = (menuId: BroadcastsMenuState) => {
+		console.log(`Selected menu: ${menuId}`);
+	};
+
+	return (
+		<Menu
+			menuItems={BROADCASTS_MENU_ITEMS}
+			title="📧 Resend CLI - Broadcasts Menu"
+			onSelect={(menuId) => {
+				handleMenuSelect(menuId);
+			}}
+			onExit={() => {
+				onExit();
+			}}
+		/>
+	);
+};
