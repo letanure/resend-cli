ec2abcf4c423e17c9e444c75bae8175222d92ad2
letanure <letanure@gmail.com>
2025-07-08 15:05:04 +0200

    fix: use right contact delete form

--- src/modules/contacts/AppContacts.tsx
+++ src/modules/contacts/AppContacts.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ContactCreateForm } from './ContactCreateForm.js';
-import { ContactDeleteForm } from './ContactDeleteForm.js';
+import { DeleteContactForm as ContactDeleteForm } from './delete/Form.js';
 import { ContactListForm } from './ContactListForm.js';
 import { ContactRetrieveForm } from './ContactRetrieveForm.js';
 import { ContactsMenu, ContactsMenuState, type ContactsMenuState as ContactsMenuStateType } from './ContactsMenu.js';
